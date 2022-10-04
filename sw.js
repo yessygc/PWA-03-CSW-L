7d71df1206cfd2f10d9333648722a40891b44da1
yessygc <yessgarciacampos@gmail.com>
2022-10-04 02:11:11 -0500

    SW: Activate

--- sw.js
+++ sw.js
@@ -5,7 +5,16 @@ self.addEventListener('install', event => {
 
     //DESCARGAR ASSETS
     //Creamos un cache
-    console.log('SW: Instalando SW');
+    console.log('SW: Instalando SW!');
 
 
+});
+
+
+//Cuando el SW toma el control de la APP
+self.addEventListener('activate', event => {
+
+    // Borrar cache viejo
+
+    console.log('SW2: Activo y Listo para controlar la app');
 });
